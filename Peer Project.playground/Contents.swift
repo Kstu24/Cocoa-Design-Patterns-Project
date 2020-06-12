7a351978282cfb861c968a897f0c4f6d005baf6d
Kevin Stewart <kistewart419@gmail.com>
2020-06-11 23:08:40 -0400

    Peer code review

---
+++ Peer Project.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+//Objective challenge:
+// Pair off, and with a peer, each choose a design pattern and think of an instance you’ve used it code you’ve written or have seen it used in UIKit.
+
+// Project Name - Astronomy
+// MarsPhoto.swift - Uses Codable & JSONDecoder,
+
+
+
+// Objective challenge:
+// Look through the documentation and find at least one UIKit class that uses the singleton pattern.
+ 
+class Singleton {
+    static let sharedInstance: Singleton = {
+        let instance = Singleton()
+        // setup code
+        return instance
+    }()
+}
+    
+
+// Objective challenge:
+// Think about a time we’ve used the facade pattern before. Are there places that we could have used it to write better code but didn’t? Discuss with a peer.
+
+// Project Name - Astronomy
+// MarsRoverClient.swift
+// class MarsRoverClient
+
+
+
+// Objective challenge:
+// Write code to post a notification when a button is tapped indicating a bell has rung. In another class, sign up to be notified when this notification is posted, and make sure it works.
+
+"https://github.com/Kstu24/ios-journal-coredata"
+
+// Objective challenge:
+// Add support for state restoration to another app you’ve built
+
+"https://github.com/Kstu24/ios-journal-coredata"
+
+
